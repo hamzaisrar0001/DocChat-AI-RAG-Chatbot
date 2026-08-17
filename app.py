9b2a5e66bfbf7e9f4b08bb321ea6523043f9e26e
Muhammad Hamza Israr <161688978+hamzaisrar0001@users.noreply.github.com>
2026-08-17 14:25:00 +0500

    Update app.py

--- app.py
+++ app.py
@@ -465,7 +465,7 @@ with st.sidebar:
     </div>
     """, unsafe_allow_html=True)
 
- st.markdown('<div class="section-label">Document</div>', unsafe_allow_html=True)
+    st.markdown('<div class="section-label">Document</div>', unsafe_allow_html=True)
 
     if st.session_state.collection:
         sidebar_file = st.file_uploader(
